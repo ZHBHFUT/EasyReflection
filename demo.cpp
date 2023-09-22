96a716b611c9d24325c61063465281ff0611009f
ZHANG Bing <zhangbing@hfut.edu.cn>
2023-09-22 16:52:36 +0800

    adjust order of some lines

--- demo.cpp
+++ demo.cpp
@@ -31,34 +31,36 @@ int main()
     using namespace EasyLib;
 
     // 1) for aggregate type
-    Foo foo{};
     constexpr auto nfield_Foo = member_count<Foo>; //--> 3
     using field_types_Foo     = member_types<Foo>; //--> Types<int,char,float>
     auto&& field_offsets_Foo  = member_offsets<Foo>(); //--> std::array<size_t,3>{0,4,8}
     using type_Foo_a1         = member_type<Foo, 0>; //--> char
-    member_of<Foo, 2>(foo) = 1; //--> foo={0,0,1}, i.e. foo.a2=1
-
+    
     static_assert(nfield_Foo == 3);
     static_assert(std::is_same_v<field_types_Foo, Types<int, char, float>>);
     static_assert(member_count<Foo> == 3);
     assert((field_offsets_Foo[0] == offsetof(Foo, a0)));
     assert((field_offsets_Foo[1] == offsetof(Foo, a1)));
     assert((field_offsets_Foo[2] == offsetof(Foo, a2)));
+    
+    Foo foo{};
+    member_of<Foo, 2>(foo) = 1; //--> foo={0,0,1}, i.e. foo.a2=1
     assert((member_offset<Foo, 1>() == offsetof(Foo, a1)));
 
     // 2) for non-aggregated type
-    Bar bar{};
     constexpr auto nfield_Bar = member_count<Bar>; //--> 3
     using field_types_Bar     = member_types<Bar>; //--> Types<int,char,float>
     auto&& field_offsets_Bar  = member_offsets<Bar>(); //--> std::array<size_t,3>{0,4,8}
     using type_Bar_a1         = member_type<Bar,2>; //--> float
-    member_of<Bar, 2>(bar) = 1; //--> bar={0,0,1}, i.e. bar.a2=1
-
+    
     static_assert(nfield_Bar == 3);
     static_assert(std::is_same_v<field_types_Bar, Types<int, char, float>>);
     assert((field_offsets_Bar[0] == offsetof(Foo, a0)));
     assert((field_offsets_Bar[1] == offsetof(Foo, a1)));
     assert((field_offsets_Bar[2] == Bar::offset_a2()));
+    
+    Bar bar{};
+    member_of<Bar, 2>(bar) = 1; //--> bar={0,0,1}, i.e. bar.a2=1
     assert((member_of<Bar, 2>(bar) == bar.get_a2()));
 
     return 0;
